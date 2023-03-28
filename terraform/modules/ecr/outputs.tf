746fe6eb09fd29142b91292daa49ad17ff8fff37
takasu-atsuki <jill.perry0208@ilcoud.com>
2023-03-28 23:06:06 +0900

    test

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,7 +1,7 @@
 output "ecr_repo_app" {
-  value = aws_ecr_repository.app
+  value = aws_ecr_repository.app.name
 }
 
 output "ecr_repo_web" {
-  value = aws_ecr_repository.web
+  value = aws_ecr_repository.web.name
 }
